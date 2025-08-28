35f66b174d3bf4871111d563139ee96e4fbed665
Sonia Chaboud <sonia.chaboud@gmail.com>
2025-08-28 17:40:06 +0200

    next button

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,17 +1,26 @@
 class ExercisesController < ApplicationController
   def index
+    @exercises = Exercise.all
   end
 
   def new
+    @exercise = Exercise.new
   end
 
   def create
     @exercise = Exercise.new(exercise_params)
     @exercise.save
+    redirect_to exercise_path(@exercise)
   end
 
   def show
     @exercise = Exercise.order("RANDOM()").first
+  end
+
+  def destroy
+    @exercise = Exercise.find(params[:id])
+    @exercise.destroy
+    redirect_to exercises_path
   end
 
   private
